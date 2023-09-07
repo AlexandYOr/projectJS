49394eb8cfcfb956127571de08307f222572b101
AlexandYOr <avo01121997@yandex.ru>
2023-09-07 21:55:06 +0300

    Lesson01 new files

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log ("Hello World!")
+const title = "JS";
+const screenPrice = 100;
+const rollback = "Exit";
+const fullPrice = 1000;
+const adaptive = true;
+const screens = 2;
+console.log("Good day to die");
+alert("Hello World!");
