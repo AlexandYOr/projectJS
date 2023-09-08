3ccaa5afa22d2f795a156bcc72d4c273e4cc658a
AlexandYOr <avo01121997@yandex.ru>
2023-09-09 00:19:14 +0300

    add forEach loop

--- hard/script.js
+++ hard/script.js
@@ -1,15 +1,17 @@
 const num = 266219;
 const numText = num.toString();
 const arrayNum = numText.split("");
-
 let result = 1;
-for (let i = 0;  i < arrayNum.length; i++) {
-    result = result * arrayNum[i]
-}
+
+arrayNum.forEach((arrayElement) => result = result * arrayElement)
 console.log(result)
 
+// for (let i = 0;  i < arrayNum.length; i++) {
+//     result = result * arrayNum[i]
+// }
+// console.log(result)
+
 // let arrayIndex = 0; // выводит значение массива
-// let result = 1;
 // while (arrayIndex < arrayNum.length) {
 //    result = result * arrayNum[arrayIndex];
 //     ++arrayIndex;
