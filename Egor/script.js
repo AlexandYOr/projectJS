8b0afe8ad7a524d1d08e234a698deabdea9828ca
AlexandYOr <avo01121997@yandex.ru>
2023-09-09 23:52:19 +0300

    new ex from Egor

--- Egor/script.js
+++ Egor/script.js
@@ -9,8 +9,21 @@
 // });
 // console.log(splitElement.join(""));
 
-const string = "computer";
-const arrayString = [];
-for (let i = string.length - 1; i >= 0; i--) {arrayString.push(string[i])
-};
-console.log(arrayString.join("").split("").reverse().join(""));
+// const string = "computer";
+// const arrayString = [];
+// for (let i = string.length - 1; i >= 0; i--) {arrayString.push(string[i])
+// };
+// console.log(arrayString.join("").split("").reverse().join(""));
+
+// for (i = 1; i <=15; i++) {
+//     console.log(i % 2 == 0)
+// }
+
+const array = [11, 22, 33, 44, 55];
+let arrayResult = 0;
+array.forEach((arrayElement) => {
+   arrayResult = arrayResult + arrayElement **2
+});
+
+console.log(arrayResult);
+
