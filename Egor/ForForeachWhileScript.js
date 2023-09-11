07fe0901cf014e31c3856957007ffbd4a8d62df8
AlexandYOr <avo01121997@yandex.ru>
2023-09-11 21:42:16 +0300

    complete Egor Lesson

---
+++ Egor/ForForeachWhileScript.js
@@ -0,0 +1,29 @@
+// for (let i = 1; i <= 20; i++) {
+// console.log(i)
+// };
+
+// const splitedString = "computer".split(""); // создали массив
+// const splitElement = []; // результат цикла
+
+// splitedString.forEach((i) => {splitElement.unshift(i)
+// });
+// console.log(splitElement.join(""));
+
+// const string = "computer";
+// const arrayString = [];
+// for (let i = string.length - 1; i >= 0; i--) {arrayString.push(string[i])
+// };
+// console.log(arrayString.join("").split("").reverse().join(""));
+
+// for (i = 1; i <=15; i++) {
+//     console.log(i % 2 == 0)
+// }
+
+// const array = [11, 22, 33, 44, 55];
+// let arrayResult = 0;
+// array.forEach((arrayElement) => {
+//    arrayResult = arrayResult + arrayElement **2
+// });
+
+// console.log(arrayResult);
+
--- Egor/script.js
+++ Egor/script.js
@@ -1,29 +1,18 @@
-// for (let i = 1; i <= 20; i++) {
-// console.log(i)
-// };
+const fruitPrice = {
+    apple: 25,
+    apricot: 30,
+    pinapple: 50,
+    tomato: 10,
+    cucumber: 200
+} // найти самый дорогой и вывести в консоль ключ.
+let topFruitPrice = 0;
+let topFruitPriceK = "";
+for (let k in fruitPrice) {
+    topFruitPrice = topFruitPrice > fruitPrice[k] ? topFruitPrice : fruitPrice[k];
+    topFruitPriceK = topFruitPrice > fruitPrice[k] ? topFruitPrice : k;
+} 
+console.log(topFruitPrice, topFruitPriceK);
+const arrayFruitPrices = Object.values(fruitPrice) ;
+console.log(Math.max(...arrayFruitPrices))
 
-// const splitedString = "computer".split(""); // создали массив
-// const splitElement = []; // результат цикла
-
-// splitedString.forEach((i) => {splitElement.unshift(i)
-// });
-// console.log(splitElement.join(""));
-
-// const string = "computer";
-// const arrayString = [];
-// for (let i = string.length - 1; i >= 0; i--) {arrayString.push(string[i])
-// };
-// console.log(arrayString.join("").split("").reverse().join(""));
-
-// for (i = 1; i <=15; i++) {
-//     console.log(i % 2 == 0)
-// }
-
-const array = [11, 22, 33, 44, 55];
-let arrayResult = 0;
-array.forEach((arrayElement) => {
-   arrayResult = arrayResult + arrayElement **2
-});
-
-console.log(arrayResult);
 
