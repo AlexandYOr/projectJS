e0f9227feb5e284b57f53040c70c5941fbe97f5a
AlexandYOr <avo01121997@yandex.ru>
2023-09-11 20:37:56 +0300

    complete lesson03hard

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
-const title = "JS";
-const screenPrice = 100;
-const rollback = 50;
-const fullPrice = 1000;
-const adaptive = true;
-const screens = "Простые, Сложные, Интерактивные";
-console.log(typeof title, typeof fullPrice, typeof adaptive);
-console.log(screens.length);
-console.log("Стоимость верстки " + screenPrice + " рублей");
-console.log("Стоимость разработки " + fullPrice + " рублей");
-console.log(screens.toLowerCase().split(", "));
-console.log(fullPrice * (rollback/100) + " %");
+const lang = confirm("Ваш язык русский?") ? "ru" : "en";
+// if (lang === "ru") {
+//     console.log("Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье");
+// } else {
+//     console.log("Monday", "Tuesday", "Wednesday", "Thurdsday", "Friday", "Saturday", "Sunday");
+// };
+// switch (lang) {
+//     case "ru":
+//         console.log("Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье");
+//         break;
+//     case "en":
+//         console.log("Monday", "Tuesday", "Wednesday", "Thurdsday", "Friday", "Saturday", "Sunday");
+//         break;
+//     default:
+//         alert("Нет доступных значений");
+//         break;
+// }
+
+const langCodes = {
+    ru: ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"],
+    en: ["Monday", "Tuesday", "Wednesday", "Thurdsday", "Friday", "Saturday", "Sunday"]
+};
+console.log(langCodes[lang].join());
+
+let namePerson = prompt("Ваше имя?") 
+namePerson === "Артем" || namePerson === "Артём" ? "Директор" : namePerson === "Александр" ? "преподователь" : "студент"
+console.log(namePerson);
