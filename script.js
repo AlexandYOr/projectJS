f336bc6d5117d673ecf69fa9de0fd6f9a9de0fcd
Olkhovskiy Aleksandr <olkhovskiy.aleksandr@FAMIL.RU>
2023-09-14 16:40:32 +0300

    add new function

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 'use strict'
+// Запрос на продолжение игры
+function confirmStep(t) {
+    let confirmQuestion = confirm(t);
+    return confirmQuestion;
+}
+// игра
+function start () {
+    const startQuestion = "угадайте число от 50 до 100"
+    if (confirmStep(startQuestion) === true) {
+        function startedGame () {
+            let n = 0;
+
+        }
+    }
+}
+
+start()
