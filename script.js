0c45ae97a7f525040f87c06d7e34d0092b11c057
AlexandYOr <avo01121997@yandex.ru>
2023-09-14 21:48:32 +0300

    lesson06hard complete

--- script.js
+++ script.js
@@ -1,35 +1,55 @@
 'use strict'
 
-let userQuestion = confirm("Угадай число от 1 до 100");
+
 
 function isNum(num) {
     return !isNaN(parseFloat(num)) && isFinite(num);
 };
 
-function hiddenNumber(n) {
-    const number = n;
+function hiddenNumber() {
+    let userQuestion = confirm("Угадай число от 1 до 100");
+    let number = parseInt(Math.random() * 100)
+    if (number === 0) {
+        number = parseInt(Math.random() * 100)
+    }
+    let attempt = 10;
 
     function startGame() {
         if (userQuestion === true) {
+            if (attempt === 0) {
+                let tryAgainQuestion = confirm("Попытки закончились. Хотите сыграть еще раз?");
+                if (tryAgainQuestion === true) {
+                    return hiddenNumber()
+                } else {
+                    return alert("До свидания!")
+                }
+            }
             let userAnswer = +prompt("Введи число");
             if (isNum(userAnswer) === true) {
+
                 if (userAnswer > number) {
-                    alert("Загаданное число меньше");
+                    attempt--
+                    alert("Загаданное число меньше осталось попыток " + attempt);
                     return startGame();
                 } else if (userAnswer < number) {
-                    alert("Загаданное число больше");
+                    attempt--
+                    alert("Загаданное число больше осталось попыток " + attempt);
                     return startGame();
                 } else if (userAnswer === number) {
-                    alert("Ура ты победил!");
+                    let finalUserAnswer = confirm("Ура ты победил! Хочешь ли сыграть еще раз?");
+                    if (finalUserAnswer === true) {
+                        return hiddenNumber()
+                    }
+                    return
                 }
             } else {
                 alert("Введи число!")
                 return startGame()
             }
         } else {
-            return startGame()
+            return alert("До свидания!")
         }
     }
     startGame()
 }
-hiddenNumber(10);
+hiddenNumber()
