d7c7de403a217703d311d19e3229ee2a42202509
Olkhovskiy Aleksandr <olkhovskiy.aleksandr@FAMIL.RU>
2023-09-15 15:59:24 +0300

    complete lesson07hard

--- script.js
+++ script.js
@@ -3,11 +3,15 @@ let day = new Date()
 let verticalWeek = "";
 for (let i = 0; i < week.length; i++) {
     if (i === day.getDay() - 1) {
-        verticalWeek += week[i] + "\n"
+        verticalWeek += week[i] + "\n";
+        document.write(`<p><b>${week[i]}</b></p>`);
     } else if (i <= 4) {
-        verticalWeek += week[i] + "\n"
+        verticalWeek += week[i] + "\n";
+        document.write(`<p>${week[i]}</p>`);
     } else if (i > 4) {
-        verticalWeek += week[i] + "\n"
+        verticalWeek += week[i] + "\n";
+        document.write(`<p><i>${week[i]}</i></p>`);
     }
 }
+
 alert(verticalWeek)
