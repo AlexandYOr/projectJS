5b9b5b152968c907355ffd2494b13e894bb12062
AlexandYOr <avo01121997@yandex.ru>
2023-09-20 00:37:47 +0300

    add day+week+month+year on lesson09hard

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
-let day = new Date()
-let verticalWeek = "";
-for (let i = 0; i < week.length; i++) {
-    if (i === day.getDay() - 1) {
-        verticalWeek += week[i] + "\n";
-        document.write(`<p><b>${week[i]}</b></p>`);
-    } else if (i <= 4) {
-        verticalWeek += week[i] + "\n";
-        document.write(`<p>${week[i]}</p>`);
-    } else if (i > 4) {
-        verticalWeek += week[i] + "\n";
-        document.write(`<p><i>${week[i]}</i></p>`);
+const month = ['Января', 'Февраля', 'Марта', 'Апреля', 'Мая', 'Июня', 'Июля', 'Августа', 'Сентября', 'Октября', 'Ноября', 'Декабря'];
+let today = new Date()
+// Сегодня Вторник, 4 февраля 2020 года, 21 час 5 минут 33 секунды'
+let messageA = "";
+function getCorrectDateA () {
+    let hour;
+    let minute;
+    let second;
+    for (let d = 0; d < week.length; d++) {
+    if (d === today.getDay() - 1) {
+        messageA = "Сегодня " + week[d]
+    }
+}
+for (let m = 0; m < month.length; m++) {
+    if (m === today.getMonth() - 1) {
+        messageA += ", " + today.getDate() + " "  + month[m] + " " + today.getFullYear() + " года,";
     }
 }
 
-alert(verticalWeek)
+}
+getCorrectDateA()
+alert(messageA)
