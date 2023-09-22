e0171f16565f9e57c1a5eae6a8154faf7eaa4321
AlexandYOr <avo01121997@yandex.ru>
2023-09-22 11:53:13 +0300

    add function for top button

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+// Изменение цвета квадрата.
+const button = document.getElementById('btn');
+let input = document.getElementById('text');
+const colorChange = function () {
+    document.querySelector("#square").style.backgroundColor = input.value
+};
+button.addEventListener('click', colorChange);
+
+
+
 
 
 
