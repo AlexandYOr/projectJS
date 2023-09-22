cf6ea13b17688023deca465bb1561186230edbbf
AlexandYOr <avo01121997@yandex.ru>
2023-09-22 13:52:44 +0300

    complete lesson11

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 // Изменение цвета квадрата.
-const button = document.getElementById('btn');
-let input = document.getElementById('text');
+const buttonTop = document.querySelector('#btn');
+let inputText = document.querySelector('#text');
+const square = document.querySelector("#square");
+const circle = document.querySelector("#circle");
 const colorChange = function () {
-    document.querySelector("#square").style.backgroundColor = input.value
+    square.style.backgroundColor = inputText.value
 };
-button.addEventListener('click', colorChange);
+buttonTop.addEventListener('click', colorChange);
 
+// Изменение свойства кнопки в кружке в квадрате
+square.querySelector('#e_btn').style.display = "none";
 
-
+//изменение свойст кружка
+let inputRange = document.querySelector('#range');
+const changeWH = function () {
+    inputRange = document.querySelector('#range')
+    circle.style.width = inputRange.value + "%"
+    circle.style.height = inputRange.value + "%"
+}
+changeWH()
+inputRange.addEventListener('input', changeWH)
 
 
 
