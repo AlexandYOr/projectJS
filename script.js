d27695b94495265eb109b8bf32afefb9d26ed660
AlexandYOr <avo01121997@yandex.ru>
2023-09-22 16:13:23 +0300

    refuctoring

--- script.js
+++ script.js
@@ -4,72 +4,62 @@ const month = ['Января', 'Февраля', 'Марта', 'Апреля', '
 const correctHours = ['час', 'часа', 'часов'];
 const correctMinutes = ['минута', 'минуты', 'минут'];
 const correctSecond = ['секунда', 'секунды', 'секунд']
-let today;
-let year;
-let numberMonth;
-let date;
-let hour;
-let minute;
-let second;
-let messageA = "";
-let messageB = "";
 function now() {
-    today = new Date()
-    year = today.getFullYear();
-    numberMonth = today.getMonth();
-    date = today.getDate();
-    day = today.getDay() - 1;
-    hour = today.getHours();
-    minute = today.getMinutes();
-    second = today.getSeconds();
+    let today = new Date()
+    let year = today.getFullYear();
+    let numberMonth = today.getMonth();
+    let date = today.getDate();
+    let day = today.getDay() - 1;
+    let hour = today.getHours();
+    let minute = today.getMinutes();
+    let second = today.getSeconds();
+    function dateA() {
+        let message = ""
+        for (let d = 0; d < week.length; d++) {
+            if (d === day) {
+                message = "Сегодня " + week[d]
+            }
+        }
+        for (let m = 0; m < month.length; m++) {
+            if (m === numberMonth) {
+                message += ", " + date + " " + month[m] + " " + year + " года, ";
+            }
+        }
+        function correctDeclination(n, declination) {
+            n = Math.abs(n) % 100;
+            let n1 = n % 10;
+            if (n > 10 && n < 20) {
+                return n + " " + declination[2];
+            };
+            if (n > 1 && n < 5) {
+                return n + " " + declination[1];
+            };
+            if (n == 1) {
+                return n + " " + declination[0];
+            };
+            return n + " " + declination[2];
+        }
+        message += correctDeclination(hour, correctHours) + " " + correctDeclination(minute, correctMinutes) + " " + correctDeclination(second, correctSecond);
+        document.getElementById('dateA').textContent = message;
+    }
+    function dateB() {
+        let message = ""
+        let todayB = [date, numberMonth + 1, year, hour, minute, second]
+        for (i = 0; i < todayB.length; i++) {
+            if (todayB[i] < 10) {
+                todayB[i] = "0" + todayB[i];
+            };
+        };
+        message = todayB[3] + ':' + todayB[4] + ':' + todayB[5] + ' ' + todayB[0] + '.' + todayB[1] + '.' + todayB[2];
+        document.getElementById('dateB').textContent = message;
+    }
+    dateA();
+    dateB();
 }
 now();
-// Сегодня Вторник, 4 февраля 2020 года, 21 час 5 минут 33 секунды'
-function dateA() {
-    for (let d = 0; d < week.length; d++) {
-        if (d === day) {
-            messageA = "Сегодня " + week[d]
-        }
-    }
-    for (let m = 0; m < month.length; m++) {
-        if (m === numberMonth) {
-            messageA += ", " + date + " " + month[m] + " " + year + " года, ";
-        }
-    }
-    function correctDeclination(n, declination) {
-        n = Math.abs(n) % 100;
-        var n1 = n % 10;
-        if (n > 10 && n < 20) {
-            return n + " " + declination[2];
-        };
-        if (n1 > 1 && n1 < 5) {
-            return n + " " + declination[1];
-        };
-        if (n1 == 1) {
-            return n + " " + declination[0];
-        };
-        return n + " " + declination[2];
-    }
-    messageA += correctDeclination(hour, correctHours) + " " + correctDeclination(minute, correctMinutes) + " " + correctDeclination(second, correctSecond);
-    document.getElementById('dateA').textContent = messageA;
-}
-// '04.02.2020 - 21:05:33'
-function dateB() {
-    let todayB = [date, numberMonth + 1, year, hour, minute, second]
-    for (i = 0; i < todayB.length; i++) {
-        if (todayB[i] < 10) {
-            todayB[i] = "0" + todayB[i];
-        };
-    };
-    messageB = todayB[3] + ':' + todayB[4] + ':' + todayB[5] + ' ' + todayB[0] + '.' + todayB[1] + '.' + todayB[2];
-    document.getElementById('dateB').textContent = messageB;
-}
-
 
 setInterval(function () {
     now()
-    dateA()
-    dateB()
 }, 1000);
 
 
