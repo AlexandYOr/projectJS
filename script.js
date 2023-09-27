44c54336d15725a0379924c931579bbdee80a57d
AlexandYOr <avo01121997@yandex.ru>
2023-09-27 23:10:38 +0300

    create and start function constructor | lesson15

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
-const DomElement = function (selector, height, width, backgroundColor, fontSize) {
+const DomElement = function (selector, height, width, backgroundColor, fontSize, position = 'static') {
     this.selector = selector;
     this.height = height;
     this.width = width;
     this.bgс = backgroundColor;
     this.fontSize = fontSize;
+    this.position = position;
+    this.createElement = function () {
+        const newElement = document.createElement('div');
+        const firstSymbol = selector[0]
+        newElement.style.cssText = `
+            height:  ${height};
+            width: ${width};
+            background-color: ${backgroundColor};
+            font-size: ${fontSize};
+            position: ${position};
+            `;
+        if (firstSymbol === '.') {
+            newElement.classList.add(selector.slice(1))
+        } else if (firstSymbol === '#') {
+            newElement.id = selector.slice(1)
+        }
+        document.body.append(newElement)
+    }
+    this.findAndMoveElement = function (e) {
+        const findElement = document.body.querySelector(selector)
+        if (e.keyCode === 39)
+            findElement.style.right = parseInt(findElement.style.right) + 10 + 'px'
+        console.log(findElement)
+    }
 }
+// const newElementWithID = new DomElement('#block', '10px', '10px', 'violet', '3px')
+const newElementWithClass = new DomElement('.block', '100px', '100px', 'pink', '3px', 'absolute')
+// newElementWithID.createElement()
+document.addEventListener("DOMContentLoaded", newElementWithClass.createElement)
+document.addEventListener("keydown", newElementWithClass.findAndMoveElement)
