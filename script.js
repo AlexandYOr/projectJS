0733b9f643d6ca2e7b19668b053634412aed7405
AlexandYOr <avo01121997@yandex.ru>
2023-09-27 15:51:15 +0300

    create func DomElement | lesson15

---
+++ script.js
@@ -0,0 +1,7 @@
+const DomElement = function (selector, height, width, backgroundColor, fontSize) {
+    this.selector = selector;
+    this.height = height;
+    this.width = width;
+    this.bgс = backgroundColor;
+    this.fontSize = fontSize;
+}
