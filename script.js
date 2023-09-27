eb40e2fc6c19932ee2b2c03a027a49c2e5ece293
AlexandYOr <avo01121997@yandex.ru>
2023-09-28 00:45:06 +0300

    create block moveElement/complete lesson | lesson15

--- script.js
+++ script.js
@@ -22,17 +22,24 @@ const DomElement = function (selector, height, width, backgroundColor, fontSize,
         }
         document.body.append(newElement)
     }
+    this.moveElement = function (e) {
+        const findElement = document.body.querySelector(selector)
+        const leftValue = parseInt(findElement.style.left || 0)
+        const topValue = parseInt(findElement.style.top || 0)
+        if (e.keyCode === 37) {
+            findElement.style.left = leftValue - 10 + 'px'
+        } else if (e.keyCode === 39) {
+            findElement.style.left = leftValue + 10 + 'px'
+        } else if (e.keyCode === 40) {
+            findElement.style.top = topValue + 10 + 'px'
+        } else if (e.keyCode === 38) {
+            findElement.style.top = topValue - 10 + 'px'
+        }
+    }
 }
-// const findAndMoveElement = function (e) {
-//         const findElement = document.body.querySelector('.block')
-//         findElement.style.right = '10px'
-//         if (e.keyCode === 39)
-//             findElement.style.right = parseInt(findElement.style.right) + 10 + 'px'
-//         console.log(findElement)
-//     }
 
 // const newElementWithID = new DomElement('#block', '10px', '10px', 'violet', '3px')
 const newElementWithClass = new DomElement('.block', '100px', '100px', 'pink', '3px', 'absolute')
 // newElementWithID.createElement()
 document.addEventListener("DOMContentLoaded", newElementWithClass.createElement)
-document.addEventListener("keydown", findAndMoveElement)
+document.addEventListener("keydown", newElementWithClass.findAndMoveElement)
