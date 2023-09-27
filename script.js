9f3b3e2d988a836e7cfe54e27ee6df1b28bef908
AlexandYOr <avo01121997@yandex.ru>
2023-09-27 23:50:55 +0300

    create listner for keyboard | lesson15

--- script.js
+++ script.js
@@ -22,15 +22,17 @@ const DomElement = function (selector, height, width, backgroundColor, fontSize,
         }
         document.body.append(newElement)
     }
-    this.findAndMoveElement = function (e) {
-        const findElement = document.body.querySelector(selector)
-        if (e.keyCode === 39)
-            findElement.style.right = parseInt(findElement.style.right) + 10 + 'px'
-        console.log(findElement)
-    }
 }
+// const findAndMoveElement = function (e) {
+//         const findElement = document.body.querySelector('.block')
+//         findElement.style.right = '10px'
+//         if (e.keyCode === 39)
+//             findElement.style.right = parseInt(findElement.style.right) + 10 + 'px'
+//         console.log(findElement)
+//     }
+
 // const newElementWithID = new DomElement('#block', '10px', '10px', 'violet', '3px')
 const newElementWithClass = new DomElement('.block', '100px', '100px', 'pink', '3px', 'absolute')
 // newElementWithID.createElement()
 document.addEventListener("DOMContentLoaded", newElementWithClass.createElement)
-document.addEventListener("keydown", newElementWithClass.findAndMoveElement)
+document.addEventListener("keydown", findAndMoveElement)
