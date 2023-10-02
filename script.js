cb1d7409fc6f9a80177bb25afe61f996d3cc36fd
AlexandYOr <avo01121997@yandex.ru>
2023-10-02 23:29:04 +0300

    create form, create cars table, add save carsinfo to localstorage, complete lesson17 | lesson 17

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let garage = JSON.parse(localStorage.getItem('garage')) ?? []; // получение данных из локал сторэдж
+const carName = document.querySelector('.car-name')
 const buttonAddCar = document.querySelector('.add-car')
 const allTypeBody = document.querySelector('#all-type-body')
 const engine = document.querySelector('#engine')
@@ -5,19 +7,31 @@ const engineType = document.querySelector('#type-of-engine')
 const allSuspension = document.querySelector('#all-suspension')
 const allTransmission = document.querySelector('#all-transmission')
 const allGearbox = document.querySelector('#all-gearbox')
+const result = document.querySelector('.result')
 
+
+// создаем класс
 class NewCar{ 
-    constructor(body, suspension, transmission, gearbox){
+    constructor(id, carName, body, suspension, transmission, gearbox){
+        this.id = id 
+        this.carName = carName;
         this.body = body;
         this.suspension = suspension;
         this.transmission = transmission;
         this.gearbox = gearbox
-    }   
+    }
+    removeCar() {
+        garage = garage.filter((car) => car.id !== this.id)
+        localStorage.setItem('garage', JSON.stringify(garage))
+        const carInPage = document.getElementById(this.id)
+        carInPage.remove()
+    } 
 }
-
+// наследники класса NewCar
 class Sedan extends NewCar{
-    constructor(body, suspension, transmission, gearbox, engine, engineType) {
-        super()
+    constructor(id, carName, body, suspension, transmission, gearbox, engine, engineType) {
+        this.id = id
+        this.carName = carName;
         this.body = body;
         this.suspension = suspension;
         this.transmission = transmission;
@@ -27,8 +41,9 @@ class Sedan extends NewCar{
     }
 }
 class Wagon extends NewCar{
-    constructor(body, suspension, transmission, gearbox, engine, engineType) {
-        super()
+    constructor(id, carName, body, suspension, transmission, gearbox, engine, engineType) {
+        this.id = id
+        this.carName = carName;
         this.body = body;
         this.suspension = suspension;
         this.transmission = transmission;
@@ -38,8 +53,9 @@ class Wagon extends NewCar{
     }
 }
 class Coupe extends NewCar{
-    constructor(body, suspension, transmission, gearbox, engine, engineType) {
-        super()
+    constructor(id, carName, body, suspension, transmission, gearbox, engine, engineType) {
+        this.id = id
+        this.carName = carName;
         this.body = body;
         this.suspension = suspension;
         this.transmission = transmission;
@@ -49,8 +65,9 @@ class Coupe extends NewCar{
     }
 }
 class HotHatch extends NewCar{
-    constructor(body, suspension, transmission, gearbox, engine, engineType) {
-        super()
+    constructor(id, carName, body, suspension, transmission, gearbox, engine, engineType) {
+        this.id = id
+        this.carName = carName;
         this.body = body;
         this.suspension = suspension;
         this.transmission = transmission;
@@ -68,10 +85,10 @@ const typeBodySelect = function(e){
         engine.style.display = 'none'
         engineType.style.display = 'none'
     }
-    console.log(e)
 }
 
 const clearForm = function() {
+    carName.value = ''
     allTypeBody.selectedIndex = 0
     engine.selectedIndex = 0
     engineType.selectedIndex = 0
@@ -81,34 +98,93 @@ const clearForm = function() {
     engine.style.display = 'none'
     engineType.style.display = 'none'
 }
+// функция в которую мы передаем свойство, возвращаем универсальный элемент
+const createCarProperty = function(propertyValue = '-') {
+    const newPropertyElement = document.createElement('span')
+    newPropertyElement.classList.add('table-value')
+    newPropertyElement.textContent = propertyValue
+    return newPropertyElement
+}
+// функция в которую мы передаем экземпляр(instance) класса (в нашем случае myNewCar).
+// функция создает новые элементы на странице
+const addCarToGarage = function(car) {
+    const newCarInGarage = document.createElement('div') // обертка
+    newCarInGarage.id = car.id
+    newCarInGarage.classList.add('table-row')
 
+    const newCarInGarageName = createCarProperty(car.carName)
+    const newCarInGarageBody = createCarProperty(car.body)
+    const newCarInGarageEngine = createCarProperty(car.engine)
+    const newCarInGarageEngineType = createCarProperty(car.engineType)
+    const newCarInGarageSuspession = createCarProperty(car.suspension)
+    const newCarInGarageTransmission = createCarProperty(car.transmission)
+    const newCarInGarageGearbox = createCarProperty(car.gearbox)
+    const deleteCarFromGarage = document.createElement('button')
+    deleteCarFromGarage.classList.add('delete-car')
+    deleteCarFromGarage.textContent = 'да'
+    deleteCarFromGarage.addEventListener('click', car.removeCar.bind(car))
+    newCarInGarage.append(newCarInGarageName, newCarInGarageBody, newCarInGarageSuspession, newCarInGarageGearbox, newCarInGarageTransmission, newCarInGarageEngine, newCarInGarageEngineType, deleteCarFromGarage)
+    result.append(newCarInGarage)
+}
+//функция в которую мы передаем свойства сущности(автомобиля в нашем случае)
+//функция возвращает экземпляр(instance) класса
+const createClassFromValues = function(carValues) {
+    const {id, carName, body, suspension, transmission, gearbox, engine, engineType} = carValues; //деструктуризация
+    let myNewCar;
+    switch (body) {
+        case 'Sedan':
+            myNewCar = new Sedan(id, carName, body, suspension, transmission, gearbox, engine, engineType)
+            break;
+        case 'Wagon':
+            myNewCar = new Wagon(id, carName, body, suspension, transmission, gearbox, engine, engineType)
+            break;
+        case 'Coupe':
+            myNewCar = new Coupe(id, carName, body, suspension, transmission, gearbox, engine, engineType)
+        case 'HotHatch':
+            myNewCar = new HotHatch(id, carName, body, suspension, transmission, gearbox, engine, engineType)
+            break;
+        default: 
+            myNewCar = new NewCar(id, carName, body, suspension, transmission, gearbox)
+            break;
+    }
+    return myNewCar
+}
+//функция в которую мы передаем массив с экземплярами(instances) классов
+//функция возвращает уникальный id.
+const calculateNewId = function(garage) {
+    const carIds = garage.map((car) => {
+        return car.id})
+    const highestId = carIds.length ? Math.max(...carIds) : 0
+    return highestId + 1
+}
+//колбэк для слушателя кнопки "В гараж"
+//функция получает значения из инпутов
+//функция создает инстанс класса используя функцию createClassFromValues
+//функция добавляет новый экземпляр в массив, локальное хранилище и на страницу.
+//функция возвращает все импуты в первоначальный вид 
 const createNewCar = function() {
+    const id = calculateNewId(garage)
+    const setCarName = carName.value
     const body = allTypeBody.value
     const selectedEngine = engine.value
     const selectedEngineType = engineType.value
     const suspension = allSuspension.value
     const transmission = allTransmission.value
     const gearbox = allGearbox.value
-    let myNewCar;
-    switch (body) {
-        case 'Sedan':
-            myNewCar = new Sedan(body, suspension, transmission, gearbox, selectedEngine, selectedEngineType)
-            break;
-        case 'Wagon':
-            myNewCar = new Wagon(body, suspension, transmission, gearbox, selectedEngine, selectedEngineType)
-            break;
-        case 'Coupe':
-            myNewCar = new Coupe(body, suspension, transmission, gearbox, selectedEngine, selectedEngineType)
-            break;
-        case 'HotHatch':
-            myNewCar = new HotHatch(body, suspension, transmission, gearbox, selectedEngine, selectedEngineType)
-            break;
-    }
-    console.log(myNewCar)
+    const myNewCar = createClassFromValues({id, carName: setCarName, body, suspension, transmission, engine: selectedEngine, engineType: selectedEngineType, gearbox})
+    garage.push(myNewCar)
+    localStorage.setItem('garage', JSON.stringify(garage))
+    addCarToGarage(myNewCar)
     clearForm()
 }
-
-
+//колбэк для слушателя DOMContentLoaded
+//функция изменяет массив свойств из локального хранилища на массив экземпляров классов при помощи функции createClassFromValues
+//функция перебирает новый массив экземпляров классов и для каждого элемента массива применяет функцию addCarToGarage
+const renderGarage = function() {
+    garage = garage.map(createClassFromValues); //перебирает массив с последующим изменением элементов массива, возвращает измененный массив
+    garage.forEach(addCarToGarage)
+}
 
 allTypeBody.addEventListener('change',typeBodySelect)
 buttonAddCar.addEventListener('click', createNewCar)
+document.addEventListener('DOMContentLoaded', renderGarage)
