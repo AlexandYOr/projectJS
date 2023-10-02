ff05b1c8f4890a46bfbabbfa666d8ab1cfeffdf6
AlexandYOr <avo01121997@yandex.ru>
2023-10-02 20:10:20 +0300

    create carform | lesson17

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const buttonAddCar = document.querySelector('.add-car')
-const buttonCreateCar = document.querySelector('.create-new-car')
+const allTypeBody = document.querySelector('#all-type-body')
+const engine = document.querySelector('#engine')
+const engineType = document.querySelector('#type-of-engine')
+const allSuspension = document.querySelector('#all-suspension')
+const allTransmission = document.querySelector('#all-transmission')
+const allGearbox = document.querySelector('#all-gearbox')
 
 class NewCar{ 
     constructor(body, suspension, transmission, gearbox){
@@ -10,35 +15,100 @@ class NewCar{
     }   
 }
 
-class Sedan extends NewCar {
-    constructor() {
-        super();
-        this.doors = 4;
-        this.trunk = 350;
-        this.body = 'sedan'
+class Sedan extends NewCar{
+    constructor(body, suspension, transmission, gearbox, engine, engineType) {
+        super()
+        this.body = body;
+        this.suspension = suspension;
+        this.transmission = transmission;
+        this.gearbox = gearbox
+        this.engine = engine;
+        this.engineType = engineType; 
     }
 }
-class Wagon extends NewCar {
-    constructor() {
-        super();
-        this.doors = 4;
-        this.trunk = 600;
-        this.body = 'Wagon'
+class Wagon extends NewCar{
+    constructor(body, suspension, transmission, gearbox, engine, engineType) {
+        super()
+        this.body = body;
+        this.suspension = suspension;
+        this.transmission = transmission;
+        this.gearbox = gearbox
+        this.engine = engine;
+        this.engineType = engineType; 
     }
 }
-class Coupe extends NewCar {
-    constructor() {
-        super();
-        this.doors = 2;
-        this.trunk = 300;
-        this.body = 'sedCoupeBodyan'
+class Coupe extends NewCar{
+    constructor(body, suspension, transmission, gearbox, engine, engineType) {
+        super()
+        this.body = body;
+        this.suspension = suspension;
+        this.transmission = transmission;
+        this.gearbox = gearbox
+        this.engine = engine;
+        this.engineType = engineType; 
     }
 }
-class HotHatch extends NewCar {
-    constructor() {
-        super();
-        this.doors = 2;
-        this.trunk = 450;
-        this.body = 'HotHatch'
+class HotHatch extends NewCar{
+    constructor(body, suspension, transmission, gearbox, engine, engineType) {
+        super()
+        this.body = body;
+        this.suspension = suspension;
+        this.transmission = transmission;
+        this.gearbox = gearbox
+        this.engine = engine;
+        this.engineType = engineType; 
     }
 }
+
+const typeBodySelect = function(e){
+    if (e.target.value) {
+        engine.style.display = 'flex'
+        engineType.style.display = 'flex'
+    } else {
+        engine.style.display = 'none'
+        engineType.style.display = 'none'
+    }
+    console.log(e)
+}
+
+const clearForm = function() {
+    allTypeBody.selectedIndex = 0
+    engine.selectedIndex = 0
+    engineType.selectedIndex = 0
+    allSuspension.selectedIndex = 0
+    allTransmission.selectedIndex = 0
+    allGearbox.selectedIndex = 0
+    engine.style.display = 'none'
+    engineType.style.display = 'none'
+}
+
+const createNewCar = function() {
+    const body = allTypeBody.value
+    const selectedEngine = engine.value
+    const selectedEngineType = engineType.value
+    const suspension = allSuspension.value
+    const transmission = allTransmission.value
+    const gearbox = allGearbox.value
+    let myNewCar;
+    switch (body) {
+        case 'Sedan':
+            myNewCar = new Sedan(body, suspension, transmission, gearbox, selectedEngine, selectedEngineType)
+            break;
+        case 'Wagon':
+            myNewCar = new Wagon(body, suspension, transmission, gearbox, selectedEngine, selectedEngineType)
+            break;
+        case 'Coupe':
+            myNewCar = new Coupe(body, suspension, transmission, gearbox, selectedEngine, selectedEngineType)
+            break;
+        case 'HotHatch':
+            myNewCar = new HotHatch(body, suspension, transmission, gearbox, selectedEngine, selectedEngineType)
+            break;
+    }
+    console.log(myNewCar)
+    clearForm()
+}
+
+
+
+allTypeBody.addEventListener('change',typeBodySelect)
+buttonAddCar.addEventListener('click', createNewCar)
